6c7fe4796478223449d7024f256874548bab5987
Román Méndez <mendezbeck@gmail.com>
2023-10-22 22:11:38 +0200

    Full Stack Foundations > Styling > Links To Public Files

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,26 @@
 import { LiveReload, Scripts } from '@remix-run/react'
 import { KCDShop } from './kcdshop.tsx'
+import { LinksFunction } from '@remix-run/node'
+import { Links } from '@remix-run/react'
 
-// 🐨 export a links function here that adds the favicon
-// 💰 It should have the following properties:
-// - rel: 'icon'
-// - type: 'image/svg+xml'
-// - href: '/favicon.svg'
+export const links: LinksFunction = () => {
+	return [
+		{
+			rel: 'icon',
+			type: 'image/svg+xml',
+			href: '/favicon.svg',
+		},
+	]
+}
 
 export default function App() {
 	return (
 		<html lang="en">
-			<head>{/* 🐨 Put Remix's <Links /> in here */}</head>
+			<head>
+				<Links />
+			</head>
 			<body>
-				<p>Hello World</p>
+				<p>Hello Román</p>
 				<Scripts />
 				<KCDShop />
 				<LiveReload />

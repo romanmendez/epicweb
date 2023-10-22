770a6d6851efaa553cf835abdff4b22a4ba1538d
Román Méndez <mendezbeck@gmail.com>
2023-10-22 22:28:27 +0200

    Full Stack Foundations > Styling > Asset Imports

--- app/root.tsx
+++ app/root.tsx
@@ -2,13 +2,14 @@ import { LiveReload, Scripts } from '@remix-run/react'
 import { KCDShop } from './kcdshop.tsx'
 import { LinksFunction } from '@remix-run/node'
 import { Links } from '@remix-run/react'
+import faviconAssetUrl from './assets/favicon.svg'
 
 export const links: LinksFunction = () => {
 	return [
 		{
 			rel: 'icon',
 			type: 'image/svg+xml',
-			href: '/favicon.svg',
+			href: faviconAssetUrl,
 		},
 	]
 }

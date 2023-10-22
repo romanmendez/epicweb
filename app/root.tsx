59d993ccca4c8895025947cef229d74d7799d13e
Román Méndez <mendezbeck@gmail.com>
2023-10-22 23:29:01 +0200

    Full Stack Foundations > Styling > Bundling CSS

--- app/root.tsx
+++ app/root.tsx
@@ -2,10 +2,19 @@ import { LiveReload, Scripts } from '@remix-run/react'
 import { KCDShop } from './kcdshop.tsx'
 import { LinksFunction } from '@remix-run/node'
 import { Links } from '@remix-run/react'
+import { cssBundleHref } from '@remix-run/css-bundle'
 import faviconAssetUrl from './assets/favicon.svg'
 import fontStyleSheet from './styles/font.css'
 import tailwindStyleSheet from './styles/tailwind.css'
+import './styles/global.css'
 
+/*
+Typescript is complaining because cssBundleHref return 'string || undefined'
+and the LinksFunction expects a return of only 'string'. To fix that we add
+a conditional statement in out array and filter for it so our final array either
+contains the cssBundleHref with the string or it doesn't.
+
+*/
 export const links: LinksFunction = () => {
 	return [
 		{
@@ -21,7 +30,13 @@ export const links: LinksFunction = () => {
 			rel: 'stylesheet',
 			href: tailwindStyleSheet,
 		},
-	]
+		cssBundleHref
+			? {
+					rel: 'stylesheet',
+					href: cssBundleHref,
+			  }
+			: null,
+	].filter(Boolean)
 }
 
 export default function App() {

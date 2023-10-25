bc9986bcd70d3bba3c6986d0542a8466c6120bc6
Román Méndez <mendezbeck@gmail.com>
2023-10-25 15:26:45 +0200

    preped for Routing section

--- app/root.tsx
+++ app/root.tsx
@@ -1,54 +1,60 @@
-import { LiveReload, Scripts } from '@remix-run/react'
-import { KCDShop } from './kcdshop.tsx'
-import { LinksFunction } from '@remix-run/node'
-import { Links } from '@remix-run/react'
+import os from 'node:os'
 import { cssBundleHref } from '@remix-run/css-bundle'
+import { json, type LinksFunction } from '@remix-run/node'
+import {
+	Links,
+	LiveReload,
+	Outlet,
+	Scripts,
+	useLoaderData,
+} from '@remix-run/react'
 import faviconAssetUrl from './assets/favicon.svg'
-import fontStyleSheet from './styles/font.css'
-import tailwindStyleSheet from './styles/tailwind.css'
-import './styles/global.css'
+import fontStylestylesheetUrl from './styles/font.css'
+import tailwindStylesheetUrl from './styles/tailwind.css'
 
-/*
-Typescript is complaining because cssBundleHref return 'string || undefined'
-and the LinksFunction expects a return of only 'string'. To fix that we add
-a conditional statement in out array and filter for it so our final array either
-contains the cssBundleHref with the string or it doesn't.
-
-*/
 export const links: LinksFunction = () => {
 	return [
-		{
-			rel: 'icon',
-			type: 'image/svg+xml',
-			href: faviconAssetUrl,
-		},
-		{
-			rel: 'stylesheet',
-			href: fontStyleSheet,
-		},
-		{
-			rel: 'stylesheet',
-			href: tailwindStyleSheet,
-		},
-		cssBundleHref
-			? {
-					rel: 'stylesheet',
-					href: cssBundleHref,
-			  }
-			: null,
+		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
+		{ rel: 'stylesheet', href: fontStylestylesheetUrl },
+		{ rel: 'stylesheet', href: tailwindStylesheetUrl },
+		cssBundleHref ? { rel: 'stylesheet', href: cssBundleHref } : null,
 	].filter(Boolean)
 }
 
+export async function loader() {
+	return json({ username: os.userInfo().username })
+}
+
 export default function App() {
+	const data = useLoaderData<typeof loader>()
 	return (
-		<html lang="en">
+		<html lang="en" className="h-full overflow-x-hidden">
 			<head>
 				<Links />
 			</head>
-			<body>
-				<p className="p-8 text-xl">Hello Román</p>
+			<body className="flex h-full flex-col justify-between bg-background text-foreground">
+				<header className="container mx-auto py-6">
+					<nav className="flex justify-between">
+						<div>
+							<div className="font-light">epic</div>
+							<div className="font-bold">notes</div>
+						</div>
+					</nav>
+				</header>
+
+				<div className="flex-1">
+					<Outlet />
+				</div>
+
+				<div className="container mx-auto flex justify-between">
+					<div>
+						<div className="font-light">epic</div>
+						<div className="font-bold">notes</div>
+					</div>
+					<p>Built with ♥️ by {data.username}</p>
+				</div>
+				<div className="h-5" />
 				<Scripts />
-				<KCDShop />
 				<LiveReload />
 			</body>
 		</html>

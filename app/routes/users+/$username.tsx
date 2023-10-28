c169a31d4d75ee0dd4c14379d23b5e0846cd037e
Román Méndez <mendezbeck@gmail.com>
2023-10-28 18:39:12 +0200

    Full Stack Foundations > Scripts > Prefetching

--- app/routes/users+/$username.tsx
+++ app/routes/users+/$username.tsx
@@ -27,7 +27,9 @@ export default function profileRoute() {
 	return (
 		<div className="container mb-48 mt-36 border-4 border-green-500">
 			<div className="text-h1">{user.name ?? user.username}</div>
-			<Link to="./notes">Notes</Link>
+			<Link to="./notes" prefetch="intent">
+				Notes
+			</Link>
 		</div>
 	)
 }

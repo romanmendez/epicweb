76865e8e35f9d1e4751b8e383252ee4bf66f3700
Román Méndez <mendezbeck@gmail.com>
2023-11-13 16:06:42 +0100

    Data Modeling > Seeding Data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,42 +3,39 @@ import fs from 'node:fs'
 
 const prisma = new PrismaClient()
 
-const user = await prisma.user.create({
+// because of our cascading effect in our schema, deleting the users will delete notes and images as well
+await prisma.user.deleteMany()
+await prisma.user.create({
 	data: {
 		username: 'romanemendez',
 		name: 'Roman',
 		email: 'mendezbeck@gmail.com',
-	},
-})
-
-const note = await prisma.note.create({
-	data: {
-		title: 'First Name',
-		content: 'Some content for the first note',
-		ownerId: user.id,
-	},
-})
-
-await prisma.note.update({
-	where: { id: note.id },
-	data: {
-		images: {
-			create: [
-				{
-					altText: 'an adorable koala cartoon illustration',
-					contentType: 'image/png',
-					blob: await fs.promises.readFile(
-						'./tests/fixtures/images/cute-koala.png',
-					),
+		id: 'd5t667', // providing our own ID will allow us to re-seed while always working on the same note
+		notes: {
+			create: {
+				title: 'Basic Koala Facts',
+				content:
+					'Koalas are found in the eucalyptus forests of eastern Australia. They have grey fur with a cream-coloured chest, and strong, clawed feet, perfect for living in the branches of trees!',
+				// nesting seed creating
+				images: {
+					create: [
+						{
+							altText: 'an adorable koala cartoon illustration',
+							contentType: 'image/png',
+							blob: await fs.promises.readFile(
+								'./tests/fixtures/images/cute-koala.png',
+							),
+						},
+						{
+							altText: 'a cartoon illustration of a koala in a tree eating',
+							contentType: 'image/png',
+							blob: await fs.promises.readFile(
+								'./tests/fixtures/images/koala-eating.png',
+							),
+						},
+					],
 				},
-				{
-					altText: 'a cartoon illustration of a koala in a tree eating',
-					contentType: 'image/png',
-					blob: await fs.promises.readFile(
-						'./tests/fixtures/images/koala-eating.png',
-					),
-				},
-			],
+			},
 		},
 	},
 })

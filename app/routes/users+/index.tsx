58739eac990416673684f526bc786377f984a886
Román Méndez <mendezbeck@gmail.com>
2023-11-21 12:38:03 +0100

    broken ORDER BY

--- app/routes/users+/index.tsx
+++ app/routes/users+/index.tsx
@@ -1,50 +1,41 @@
 import { json, redirect, type DataFunctionArgs } from '@remix-run/node'
 import { Link, useLoaderData } from '@remix-run/react'
+import { z } from 'zod'
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
 import { SearchBar } from '#app/components/search-bar.tsx'
 import { prisma } from '#app/utils/db.server.ts'
 import { cn, getUserImgSrc, useDelayedIsPending } from '#app/utils/misc.tsx'
-import { z } from 'zod'
 import { ErrorList } from '#app/components/forms.tsx'
+
+const UserSchema = z.object({
+	id: z.string(),
+	name: z.string().nullable(),
+	username: z.string(),
+	imageId: z.string().nullable(),
+})
+const UsersSchema = z.array(UserSchema)
 
 export async function loader({ request }: DataFunctionArgs) {
 	const searchTerm = new URL(request.url).searchParams.get('search')
 	if (searchTerm === '') {
 		return redirect('/users')
 	}
-	const UserSchema = z.object({
-		id: z.string(),
-		name: z.string().nullable(),
-		username: z.string(),
-		imageId: z.string().nullable(),
-	})
-	const UsersSchema = z.array(UserSchema)
 
 	const like = `%${searchTerm ?? ''}%`
 	const rawUsers = await prisma.$queryRaw`
-		SELECT 
-				User.id, 
-				User.name, 
-				User.username, 
-				UserImage.id as imageId
-		FROM 
-				User 
-		LEFT JOIN 
-				UserImage ON UserImage.userId = User.id
-		LEFT JOIN (
-				SELECT 
-						ownerId, 
-						MAX(updatedAt) as lastUpdated
-				FROM 
-						Note
-				GROUP BY 
-						ownerId
-		) as RecentNote ON User.id = RecentNote.ownerId
-		WHERE 
-				User.username LIKE ${like} OR User.name LIKE ${like}
-		ORDER BY 
-				RecentNote.lastUpdated DESC
-		LIMIT 50;
+		SELECT User.id, User.name, User.username, UserImage.id AS imageId
+		FROM User 
+		LEFT JOIN UserImage ON User.id = UserImage.userId
+		WHERE User.username LIKE ${like} 
+		OR User.name LIKE ${like};
+		ORDER BY (
+			SELECT Note.updatedAt
+			FROM Note
+			WHERE Note.ownerId = User.id
+			ORDER BY Note.updatedAt DESC
+			LIMIT 1
+		) DESC
+		LIMIT 50
 	`
 	const results = UsersSchema.safeParse(rawUsers)
 	console.log(results)

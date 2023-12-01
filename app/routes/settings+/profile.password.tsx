05b83523a16e609f6e218b1e6f9f2d3eec1c4a9b
Román Méndez <mendezbeck@gmail.com>
2023-12-01 18:16:18 +0100

    fix password route to match playground

--- app/routes/settings+/profile.password.tsx
+++ app/routes/settings+/profile.password.tsx
@@ -1,273 +1,138 @@
 import { conform, useForm } from '@conform-to/react'
 import { getFieldsetConstraint, parse } from '@conform-to/zod'
 import { json, redirect, type DataFunctionArgs } from '@remix-run/node'
-import { Link, useFetcher, useLoaderData } from '@remix-run/react'
+import { Form, Link, useActionData } from '@remix-run/react'
 import { AuthenticityTokenInput } from 'remix-utils/csrf/react'
 import { z } from 'zod'
 import { ErrorList, Field } from '#app/components/forms.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { Icon } from '#app/components/ui/icon.tsx'
 import { StatusButton } from '#app/components/ui/status-button.tsx'
+import {
+	getPasswordHash,
+	requireUserId,
+	verifyUserPassword,
+} from '#app/utils/auth.server.ts'
 import { validateCSRFToken } from '#app/utils/csrf.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
-import {
-	getUserImgSrc,
-	invariantResponse,
-	useDoubleCheck,
-} from '#app/utils/misc.tsx'
-import {
-	EmailSchema,
-	NameSchema,
-	UsernameSchema,
-} from '#app/utils/user-validation.ts'
+import { useIsPending } from '#app/utils/misc.tsx'
+import { PasswordSchema } from '#app/utils/user-validation.ts'
 
-const ProfileFormSchema = z.object({
-	name: NameSchema.optional(),
-	username: UsernameSchema,
-	email: EmailSchema,
-})
+export const handle = {
+	breadcrumb: <Icon name="dots-horizontal">Password</Icon>,
+}
 
-export async function loader({ request }: DataFunctionArgs) {
-	const userId = 'some_user_id' // we'll take care of this next
-	const user = await prisma.user.findUnique({
-		where: { id: userId },
-		select: {
-			id: true,
-			name: true,
-			username: true,
-			email: true,
-			image: {
-				select: { id: true },
-			},
-		},
+const ChangePasswordForm = z
+	.object({
+		currentPassword: PasswordSchema,
+		newPassword: PasswordSchema,
+		confirmNewPassword: PasswordSchema,
 	})
-
-	invariantResponse(user, 'User not found', { status: 404 })
-
-	return json({ user })
-}
-
-type ProfileActionArgs = {
-	request: Request
-	userId: string
-	formData: FormData
-}
-const profileUpdateActionIntent = 'update-profile'
-const deleteDataActionIntent = 'delete-data'
+	.superRefine(({ confirmNewPassword, newPassword }, ctx) => {
+		if (confirmNewPassword !== newPassword) {
+			ctx.addIssue({
+				path: ['confirmNewPassword'],
+				code: 'custom',
+				message: 'The passwords must match',
+			})
+		}
+	})
 
 export async function action({ request }: DataFunctionArgs) {
-	const userId = 'some_user_id' // we'll take care of this next
+	const userId = await requireUserId(request)
 	const formData = await request.formData()
 	await validateCSRFToken(formData, request.headers)
-	const intent = formData.get('intent')
-	switch (intent) {
-		case profileUpdateActionIntent: {
-			return profileUpdateAction({ request, userId, formData })
-		}
-		case deleteDataActionIntent: {
-			return deleteDataAction({ request, userId, formData })
-		}
-		default: {
-			throw new Response(`Invalid intent "${intent}"`, { status: 400 })
-		}
-	}
-}
-
-export default function EditUserProfile() {
-	const data = useLoaderData<typeof loader>()
-
-	return (
-		<div className="flex flex-col gap-12">
-			<div className="flex justify-center">
-				<div className="relative h-52 w-52">
-					<img
-						src={getUserImgSrc(data.user.image?.id)}
-						alt={data.user.username}
-						className="h-full w-full rounded-full object-cover"
-					/>
-					<Button
-						asChild
-						variant="outline"
-						className="absolute -right-3 top-3 flex h-10 w-10 items-center justify-center rounded-full p-0"
-					>
-						<Link
-							preventScrollReset
-							to="photo"
-							title="Change profile photo"
-							aria-label="Change profile photo"
-						>
-							<Icon name="camera" className="h-4 w-4" />
-						</Link>
-					</Button>
-				</div>
-			</div>
-			<UpdateProfile />
-
-			<div className="col-span-6 my-6 h-1 border-b-[1.5px] border-foreground" />
-			<div className="col-span-full flex flex-col gap-6">
-				<div>
-					<Link to="password">
-						<Icon name="dots-horizontal">Change Password</Icon>
-					</Link>
-				</div>
-				<div>
-					<a
-						download="my-epic-notes-data.json"
-						href="/resources/download-user-data"
-					>
-						<Icon name="download">Download your data</Icon>
-					</a>
-				</div>
-				<DeleteData />
-			</div>
-		</div>
-	)
-}
-
-async function profileUpdateAction({ userId, formData }: ProfileActionArgs) {
 	const submission = await parse(formData, {
 		async: true,
-		schema: ProfileFormSchema.superRefine(async ({ email, username }, ctx) => {
-			const existingUsername = await prisma.user.findUnique({
-				where: { username },
-				select: { id: true },
-			})
-			if (existingUsername && existingUsername.id !== userId) {
-				ctx.addIssue({
-					path: ['username'],
-					code: 'custom',
-					message: 'A user already exists with this username',
-				})
-			}
-			const existingEmail = await prisma.user.findUnique({
-				where: { email },
-				select: { id: true },
-			})
-			if (existingEmail && existingEmail.id !== userId) {
-				ctx.addIssue({
-					path: ['email'],
-					code: 'custom',
-					message: 'A user already exists with this email',
-				})
-			}
-		}),
+		schema: ChangePasswordForm.superRefine(
+			async ({ currentPassword, newPassword }, ctx) => {
+				if (currentPassword && newPassword) {
+					const user = await verifyUserPassword({ id: userId }, currentPassword)
+					if (!user) {
+						ctx.addIssue({
+							path: ['currentPassword'],
+							code: 'custom',
+							message: 'Incorrect password.',
+						})
+					}
+				}
+			},
+		),
 	})
+	// clear the payload so we don't send the password back to the client
+	submission.payload = {}
 	if (submission.intent !== 'submit') {
+		// clear the value so we don't send the password back to the client
+		submission.value = undefined
 		return json({ status: 'idle', submission } as const)
 	}
 	if (!submission.value) {
 		return json({ status: 'error', submission } as const, { status: 400 })
 	}
 
-	const data = submission.value
+	const { newPassword } = submission.value
 
 	await prisma.user.update({
 		select: { username: true },
 		where: { id: userId },
 		data: {
-			name: data.name,
-			username: data.username,
-			email: data.email,
+			password: {
+				update: {
+					hash: await getPasswordHash(newPassword),
+				},
+			},
 		},
 	})
 
-	return json({ status: 'success', submission } as const)
+	return redirect(`/settings/profile`)
 }
 
-function UpdateProfile() {
-	const data = useLoaderData<typeof loader>()
-
-	const fetcher = useFetcher<typeof profileUpdateAction>()
+export default function ChangePasswordRoute() {
+	const actionData = useActionData<typeof action>()
+	const isPending = useIsPending()
 
 	const [form, fields] = useForm({
-		id: 'edit-profile',
-		constraint: getFieldsetConstraint(ProfileFormSchema),
-		lastSubmission: fetcher.data?.submission,
+		id: 'signup-form',
+		constraint: getFieldsetConstraint(ChangePasswordForm),
+		lastSubmission: actionData?.submission,
 		onValidate({ formData }) {
-			return parse(formData, { schema: ProfileFormSchema })
+			return parse(formData, { schema: ChangePasswordForm })
 		},
-		defaultValue: {
-			username: data.user.username,
-			name: data.user.name ?? '',
-			email: data.user.email,
-		},
+		shouldRevalidate: 'onBlur',
 	})
 
 	return (
-		<fetcher.Form method="POST" {...form.props}>
+		<Form method="POST" {...form.props} className="mx-auto max-w-md">
 			<AuthenticityTokenInput />
-			<div className="grid grid-cols-6 gap-x-10">
-				<Field
-					className="col-span-3"
-					labelProps={{
-						htmlFor: fields.username.id,
-						children: 'Username',
-					}}
-					inputProps={conform.input(fields.username)}
-					errors={fields.username.errors}
-				/>
-				<Field
-					className="col-span-3"
-					labelProps={{ htmlFor: fields.name.id, children: 'Name' }}
-					inputProps={conform.input(fields.name)}
-					errors={fields.name.errors}
-				/>
-				<Field
-					className="col-span-3"
-					labelProps={{ htmlFor: fields.email.id, children: 'Email' }}
-					inputProps={conform.input(fields.email)}
-					errors={fields.email.errors}
-				/>
-			</div>
-
-			<ErrorList errors={form.errors} id={form.errorId} />
-
-			<div className="mt-8 flex justify-center">
+			<Field
+				labelProps={{ children: 'Current Password' }}
+				inputProps={conform.input(fields.currentPassword, { type: 'password' })}
+				errors={fields.currentPassword.errors}
+			/>
+			<Field
+				labelProps={{ children: 'New Password' }}
+				inputProps={conform.input(fields.newPassword, { type: 'password' })}
+				errors={fields.newPassword.errors}
+			/>
+			<Field
+				labelProps={{ children: 'Confirm New Password' }}
+				inputProps={conform.input(fields.confirmNewPassword, {
+					type: 'password',
+				})}
+				errors={fields.confirmNewPassword.errors}
+			/>
+			<ErrorList id={form.errorId} errors={form.errors} />
+			<div className="grid w-full grid-cols-2 gap-6">
+				<Button variant="secondary" asChild>
+					<Link to="..">Cancel</Link>
+				</Button>
 				<StatusButton
 					type="submit"
-					size="wide"
-					name="intent"
-					value={profileUpdateActionIntent}
-					status={
-						fetcher.state !== 'idle'
-							? 'pending'
-							: fetcher.data?.status ?? 'idle'
-					}
+					status={isPending ? 'pending' : actionData?.status ?? 'idle'}
 				>
-					Save changes
+					Change Password
 				</StatusButton>
 			</div>
-		</fetcher.Form>
-	)
-}
-
-async function deleteDataAction({ userId }: ProfileActionArgs) {
-	await prisma.user.delete({ where: { id: userId } })
-	return redirect('/')
-}
-
-function DeleteData() {
-	const dc = useDoubleCheck()
-
-	const fetcher = useFetcher<typeof deleteDataAction>()
-	return (
-		<div>
-			<fetcher.Form method="POST">
-				<AuthenticityTokenInput />
-				<StatusButton
-					{...dc.getButtonProps({
-						type: 'submit',
-						name: 'intent',
-						value: deleteDataActionIntent,
-					})}
-					variant={dc.doubleCheck ? 'destructive' : 'default'}
-					status={fetcher.state !== 'idle' ? 'pending' : 'idle'}
-				>
-					<Icon name="trash">
-						{dc.doubleCheck ? `Are you sure?` : `Delete all your data`}
-					</Icon>
-				</StatusButton>
-			</fetcher.Form>
-		</div>
+		</Form>
 	)
 }

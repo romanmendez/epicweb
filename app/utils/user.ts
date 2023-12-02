f0dc609f49933011ba7c7aff17ef7bf1748bf5b3
Román Méndez <mendezbeck@gmail.com>
2023-12-02 14:26:53 +0100

    fix bug in useOptinalUser

--- app/utils/user.ts
+++ app/utils/user.ts
@@ -8,7 +8,7 @@ export function useOptionalUser() {
 
 export function useUser() {
 	const maybeUser = useOptionalUser()
-	if (maybeUser) {
+	if (!maybeUser) {
 		throw new Error(
 			'No user found in root loader, but user is required by useUser. If user is optional, try useOptionalUser instead.',
 		)

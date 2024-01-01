a2a8a79dd966ef1a63b68305051abc7f1cd520ff
Román Méndez <mendezbeck@gmail.com>
2024-01-01 16:10:20 +0100

    simplyfied failing createRemixStub test

--- app/routes/users+/$username.test.tsx
+++ app/routes/users+/$username.test.tsx
@@ -1,52 +1,27 @@
 /**
  * @vitest-environment jsdom
  */
-import { faker } from '@faker-js/faker'
 import { json } from '@remix-run/node'
 import { createRemixStub } from '@remix-run/testing'
 import { render, screen } from '@testing-library/react'
-import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
 import { test } from 'vitest'
-import { default as UsernameRoute, type loader } from './$username.tsx'
-
-function createFakeUser() {
-	const user = {
-		id: faker.string.uuid(),
-		name: faker.person.fullName(),
-		username: faker.internet.userName(),
-		createdAt: faker.date.past(),
-		image: {
-			id: faker.string.uuid(),
-		},
-	}
-	return user
-}
+import { default as Greeting, type loader } from './$username.tsx'
 
 test('The user profile when not logged in as self', async () => {
-	const user = createFakeUser()
 	const App = createRemixStub([
 		{
-			path: '/users/:username',
-			Component: UsernameRoute,
+			path: '/',
+			Component: Greeting,
 			loader(): Awaited<ReturnType<typeof loader>> {
 				return json({
-					user,
-					userJoinedDisplay: user.createdAt.toLocaleDateString(),
+					greeting: 'Hello, world!',
 				})
 			},
 		},
 	])
 
-	const routeUrl = `/users/${user.username}`
-	render(<App initialEntries={[routeUrl]} />, {
-		wrapper: ({ children }) => (
-			<AuthenticityTokenProvider token="test-csrf-token">
-				{children}
-			</AuthenticityTokenProvider>
-		),
-	})
+	render(<App />)
 
-	await screen.findByRole('heading', { level: 1, name: user.name })
-	await screen.findByRole('img', { name: user.name })
-	await screen.findByRole('link', { name: `${user.name}'s notes` })
+	const greeting = await screen.findByRole('status')
+	console.log(greeting)
 })
